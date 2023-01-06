2e3e35953a6a43f0a7a13b489dc503808da45406
helmut cardenas <cardenas.hcconstrucciones@gmail.com>
2023-01-05 22:52:58 -0300

    creacion de new

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,2 +1,11 @@
 class ArticleController < ApplicationController
+    #GET /articles
+    def home
+        @articles = article.all
+    end
+
+    #GET /articles/:id
+    def show
+        @article = Articles.find(params[:id])
+    end
 end
